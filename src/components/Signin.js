9460e7d772eb132086a5a23d4c989a1a049ce155
coddinggilm <coddinggilm@gmail.com>
2021-01-25 20:30:52 +0000

    fixed fetching route

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -21,7 +21,7 @@ class Signin extends Component {
 
     onSubmitSigning = () => {
         // event.preventDefault();
-        fetch('http://localhost:3000/signin', {
+        fetch('https://serene-spire-38055.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
